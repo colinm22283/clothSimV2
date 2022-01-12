77b98f27add05d5c090fd7365c43978823ce5bf4
Colin M <colinm22283@isd273.org>
2022-01-12 14:51:35 -0600

    1.0

--- scripts/source/simConfig.cpp
+++ scripts/source/simConfig.cpp
@@ -6,7 +6,7 @@ float SimConfig::moveMult = 0.001;
 float SimConfig::lineLength = 9;
 float SimConfig::decay = 0.6;
 float SimConfig::maxForce = 0.5;
-float SimConfig::gravity = 0.01;
+float SimConfig::gravity = 0.005;
 
 float SimConfig::vx = 0;
 float SimConfig::vy = 0;
